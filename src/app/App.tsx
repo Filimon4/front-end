bb8d1039e779a588ab74212e18624d8ab44caceb
FILIMON <f.rykov@bk.ru>
2024-09-21 12:33:47 +0300

    change

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,8 @@
+import axios from "axios"
 import Routing from "./routes"
 
 const App = () => {
+  axios.get(`http://${import.meta.env.VITE_BACKEND_HOST}:${import.meta.env.VITE_BACKEND_PORT}/user/cookie`)
   return (
     <>
       <Routing />
